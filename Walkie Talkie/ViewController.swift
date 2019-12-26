9315cf1b34fbdfa82f277bb4d8d982758f1e97fd
Sebastien Menozzi <sebastien.menozzi@hotmail.fr>
2019-12-26 20:29:04 +0100

    Initial Commit

---
+++ Walkie Talkie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Walkie Talkie
+//
+//  Created by Sebastien Menozzi on 26/12/2019.
+//  Copyright © 2019 Sebastien Menozzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
