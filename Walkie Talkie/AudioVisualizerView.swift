98d28762472d7474db10083df9a83081cd0ac1e4
Sebastien Menozzi <sebastien.menozzi@hotmail.fr>
2019-12-27 20:34:33 +0100

    first commit

---
+++ Walkie Talkie/AudioVisualizerView.swift
@@ -0,0 +1,9 @@
+//
+//  AudioVisualizerView.swift
+//  Walkie Talkie
+//
+//  Created by Sebastien Menozzi on 27/12/2019.
+//  Copyright © 2019 Sebastien Menozzi. All rights reserved.
+//
+
+import Foundation
---
+++ Walkie Talkie/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Walkie Talkie
+//
+//  Created by Sebastien Menozzi on 26/12/2019.
+//  Copyright © 2019 Sebastien Menozzi. All rights reserved.
+//
+
+import Foundation
